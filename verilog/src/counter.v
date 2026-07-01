e6864899d89d3ea4e429f951841c32883bcb9676
Andrea Sannino <andreasannino09@gmail.com>
2026-07-01 17:01:21 +0200

    try counter.v

--- verilog/src/counter.v
+++ verilog/src/counter.v
@@ -4,15 +4,18 @@
  * A simple 8-bit counter module. The counter increments on each rising edge of the clock and resets to zero when the reset signal is low.
  * This is a try for a simple counter module for the SkyWater 130nm process.
 */
+`default_nettype none
+`ifndef __COUNTER__
+`define __COUNTER__
 module counter (
-    input wire clk_i;
-    input wire rst_n_i;
+    input  clk_i;
+    input  rst_n_i;
     output wire [7:0] count_o
 );
 
     reg [7:0] count;
 
-    always @(posedge clk_i or negedge rst_n_i) begin
+    always @(posedge clk_i) begin
         if (!rst_n_i) begin
             count <= 8'b0;
         end else begin
@@ -24,3 +27,5 @@ module counter (
   
 
 endmodule // counter
+`endif
+`default_nettype wire
